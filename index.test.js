52cf9df313ffe2252c395e53333d10aff6d0c4e6
Shana <shanaallenholder@hotmail.co.uk>
2024-08-05 16:42:42 +0100

    Added tests for Band and Musician database

--- index.test.js
+++ index.test.js
@@ -3,20 +3,46 @@ const { execSync } = require('child_process');
 execSync('npm install');
 execSync('npm run seed');
 
+
 const request = require("supertest")
 const { db } = require('./db/connection');
 const { Musician } = require('./models/index')
 const app = require('./src/app');
 const seedMusician = require("./seedData");
+const { Json } = require('sequelize/lib/utils');
 
 
 describe('./musicians endpoint', () => {
     // Write your tests here
+    it(" Testing musicians endpoint" , async () => {
+        const response = await request(app). get("/musicians");
+        expect(response.statusCode).toBe(200);
+     
+      
+     });
+
+     it("Testing musicians data in the database", async () => {
+         const response = await request(app). get("/musicians");
+         const responseData = JSON.parse(response.text);
+         expect(responseData[0].name).toBe('Mick Jagger');
+         expect(responseData[0].instrument).toBe('Voice');
+
+        })
+  })
     
-    
+        describe('./Bands endpoint', () => {
+    it(" Testing Bands endpoint" , async () => {
+            const response = await request(app). get("/bands");
+            expect(response.statusCode).toBe(200);
+             
+              
+    })
+        
+    it("Testing Bands data in the database", async () => {
+            const response = await request(app). get("/bands");
+            const responseData = JSON.parse(response.text);
+            expect(responseData[1].name).toBe('Black Pink');
+            expect(responseData[1].genre).toBe('Pop');
+     });
 
-
-
-
-    
-})
+ })
--- src/app.js
+++ src/app.js
@@ -1,13 +1,31 @@
 const express = require("express");
 const app = express();
-const { Musician } = require("../models/index")
+const { Musician, Band } = require("../models/index")
 const { db } = require("../db/connection")
 
 const port = 3000;
 
 //TODO: Create a GET /musicians route to return all musicians 
 
+app.get("/musicians", async (req, res) => {
+  
+    const musicians = await Musician.findAll({});
+    res.json(musicians);
+    
 
+})
+
+app.get("/musicians/1" , async (req,res) => {
+
+    const musicians = await Musician.findByPk(1)
+    res.json(musicians);
+})
+
+app.get("/bands" , async (req,res) => {
+
+    const bands = await Band.findAll({});
+    res.json(bands)
+})
 
 
 
